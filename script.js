f5a31be85ba82de6c052f1fe525ed2fb9fb3cbd3
Anshu-Gondi <agondi982@gmail.com>
2024-11-10 12:52:30 +0530

    Upload project files

--- script.js
+++ script.js
@@ -73,7 +73,3 @@ function type(time) {
 }
 
 requestAnimationFrame(type);
-document.querySelector("#contact-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Thank you for contacting me! I'll get back to you soon.");
-});
